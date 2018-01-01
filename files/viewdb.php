ac9d383c7874b53b189e2d9734bdee738449c069
Chia.Wui.Lee <Chia.Wui.Lee@continental-corporation.com>
2018-01-01 11:30:09 +0800

    Implement filter for viewing questions

--- files/viewdb.php
+++ files/viewdb.php
@@ -15,29 +15,48 @@
       echo "Debug: Opened database successfully<br><br>";
    }
    
+   // Query list of category
+   $sql = "SELECT DISTINCT CATEGORY FROM QUESTION";
+   
+   $ret = $db->query($sql);
+   echo "CATEGORY FILTER= ";
+   while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
+      $category = $row['CATEGORY'];
+      echo "<a href=\"http://localhost:8000/viewdb.php?arg=cat&filter=$category\"> $category</a> | ";
+   }
+   echo "<br><br>";
+   
+   //check argument
    if (isset($_GET["arg"])) {
-	if($_GET["arg"]=="del"){
-	   $id = $_GET["id"];
-	   $sql ="DELETE FROM QUESTION WHERE ID = $id";
-	   
-	   $ret = $db->exec($sql);
-		if(!$ret){
-		   echo $db->lastErrorMsg();
-		} else {
-		   echo "Deleted successfully\n";
+		if($_GET["arg"]=="del"){
+		   $id = $_GET["id"];
+		   $sql ="DELETE FROM QUESTION WHERE ID = $id";
+		   
+		   $ret = $db->exec($sql);
+			if(!$ret){
+			   echo $db->lastErrorMsg();
+			} else {
+			   echo "Deleted successfully\n";
+			}
+		}
+		else if($_GET["arg"]=="cat"){
+			$filter = $_GET["filter"];
+			// Query list of all question filtered by category
+			$sql = "SELECT * from QUESTION WHERE CATEGORY = \"$filter\"";
 		}
 	}
-}
+	else{
+		$sql = "SELECT * from QUESTION";
+	}
+      
    
-   
-   $sql =<<<EOF
-      SELECT * from QUESTION;
-EOF;
 
    $ret = $db->query($sql);
+   $id =0;
    while($row = $ret->fetchArray(SQLITE3_ASSOC) ) {
-      echo "". $row['ID'] . ".\n";
-      //echo "QUESTION = ". $row['QUESTION'] ."\n";
+      $id++;
+	  echo "$id . ";
+      echo "QUESTION_ID = ". $row['ID'] . " |";
       echo "ANSWER = ". $row['ANSWER'] ." | ";
       echo "CATEGORY = ".$row['CATEGORY'] ." | ";
 	  $questionid = $row['ID'];
